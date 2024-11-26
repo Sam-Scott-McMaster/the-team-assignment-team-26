49cccfaca2d1f178e13a7a6e96ec8e8dac1b5124
Shayaha <jeyasees@mcmaster.ca>
2024-11-26 15:22:16 -0500

    Moved file from <source_branch> to <target_branch>

---
+++ baddie-or-not.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+help() {
+    echo "Help: This script runs different games."
+    echo "Usage: ./baddie-or-not --[help/test]"
+
+}
+
+test() {
+    #run games
+    ./hangman
+    ./wordle
+    ./maze
+    ./match
+
+    #game scores
+    hangman_score=$(cat hangman_score.txt)
+    wordle_score=$(cat wordle_score.txt)
+    maze_score=$(cat maze_score.txt)
+    match_score=$(cat match_score.txt)
+
+    #average score
+    score=$(((hangman_score + wordle_score + maze_score + match_score)/4)*100)
+
+    if [[ $score -ge 0 && $score -lt 20 ]]; then
+        echo "Cruella"
+    elif [[ $score -ge 20 && $score -lt 40 ]]; then
+        echo "Scar"
+    elif [[ $score -ge 40 && $score -lt 60 ]]; then
+        echo "Jafar"
+    elif [[ $score -ge 60 && $score -lt 80 ]]; then
+        echo "Ursula"
+    else
+        echo "Gaston"
+    fi
+}
+
+# Main logic
+if [[ "$#" -eq 0 ]]; then
+    echo "Usage: ./baddie-or-not --[help/test]"
+    exit 1
+fi
+
+for arg in "$@"; do
+    if [[ "$arg" == "--test" ]]; then
+        test
+        exit 0
+    elif [[ "$arg" == "--help" ]]; then
+        help
+        exit 0
+    else
+        echo "Usage: ./baddie-or-not --[help/test]"
+        exit 1
+    fi
+done
