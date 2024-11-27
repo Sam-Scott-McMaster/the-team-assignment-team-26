2957eb4f35799b8407a8dde0dda10469b256f876
Shayaha <jeyasees@mcmaster.ca>
2024-11-27 13:53:19 -0500

    rename a txt file made by output

--- hangman_test_script.sh
+++ hangman_test_script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #clean previous build and coverage files
-rm -f *.gcda *.gcno *.gcov hangman gcov_output.txt hangman_score.txt
+rm -f *.gcda *.gcno *.gcov hangman hangman_gcov_output.txt hangman_score.txt
 
 #compile with coverage
 gcc -o hangman hangman-test.c -fprofile-arcs -ftest-coverage -Wall
@@ -13,20 +13,20 @@ if [ $? -ne 0 ]; then
 fi
 
 #case 1: EOF
-echo -e "EOF Test Case:\n" >> gcov_output.txt
-(echo -n "" | ./hangman) >> gcov_output.txt
+echo -e "EOF Test Case:\n" >> hangman_gcov_output.txt
+(echo -n "" | ./hangman) >> hangman_gcov_output.txt
 
 #case 2: correct guesses
-echo -e "\nCorrect Guesses Test Case:\n" >> gcov_output.txt
-(echo -e "c\na\nn\nd\nEOF" | ./hangman) >> gcov_output.txt
+echo -e "\nCorrect Guesses Test Case:\n" >> hangman_gcov_output.txt
+(echo -e "c\na\nn\nd\nEOF" | ./hangman) >> hangman_gcov_output.txt
 
 #case 3: incorrect guesses
-echo -e "\nIncorrect Guesses Test Case:\n" >> gcov_output.txt
-(echo -e "z\ny\nx\nEOF" | ./hangman) >> gcov_output.txt
+echo -e "\nIncorrect Guesses Test Case:\n" >> hangman_gcov_output.txt
+(echo -e "z\ny\nx\nEOF" | ./hangman) >> hangman_gcov_output.txt
 
 #case 4: correct and incorrect guesses
-echo -e "\nMixed Guesses Test Case:\n" >> gcov_output.txt
-(echo -e "c\nz\na\nx\nEOF" | ./hangman) >> gcov_output.txt
+echo -e "\nMixed Guesses Test Case:\n" >> hangman_gcov_output.txt
+(echo -e "c\nz\na\nx\nEOF" | ./hangman) >> hangman_gcov_output.txt
 
 #generate the gcov report
 if [ -f "hangman-hangman-test.gcno" ]; then
@@ -37,5 +37,5 @@ if [ -f "hangman-hangman-test.gcda" ]; then
     mv hangman-hangman-test.gcda hangman-test.gcda
 fi
 
-gcov -b hangman-test.c >> gcov_output.txt
-cat gcov_output.txt
+gcov -b hangman-test.c >> hangman_gcov_output.txt
+cat hangman_gcov_output.txt
